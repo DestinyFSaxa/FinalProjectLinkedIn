0c1c190276161ac29075f752853c98bbc75a0493
DestinyFSaxa <df791@georgetown.edu>
2024-12-12 20:16:02 -0800

    Update main.py

--- main.py
+++ main.py
@@ -54,9 +54,10 @@ precision_sklearn = report['Using LinkedIn']['precision']
 recall_sklearn = report['Using LinkedIn']['recall']
 f1_score_sklearn = report['Using LinkedIn']['f1-score']
 
+income = st.selectbox("What is your Annual Salary?",
+options = ["Less than $10,000","10 to under $20,000","20 to under $30,000","30 to under $40,000","40 to under $50,000","50 to under $75,000","75 to under $100,000","100 to under $150,000","$150,000 or more?"]
 
-
-age = st.slider(label="Enter Age",min_value=1,max_value=97,value=7)
+age = st.slider(label="How Old Are You?",min_value=1,max_value=97,value=7)
 
 educ2 = st.selectbox("What is your Education level?",
 options = ["Less Than High School","Some High School","High School Diploma","Some College(No Degree)", "Associate Degree","College Degree","Some Graduate(No Degree)","Graduate Degree"])
